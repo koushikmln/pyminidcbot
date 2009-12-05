1ab3f631daa14be4345e20c0c4bf5909bc0c043c
andy.evil <andy.evil@881cdd00-b8e0-11dd-a80a-757f205f25d6>
2009-12-05 23:15:04 +0000

    first testing about downloading. No threads at all.

--- trunk/testbot.py
+++ trunk/testbot.py
@@ -3,11 +3,13 @@
 #testing bot - for testing new features
 from botclass import *
 from func import *
+import random
 
 class TestBot(PyBot):
     botnick='TestBot'
     HOST='10.4.20.2'
-    debug=2
+    botip='10.4.255.156'
+    debug=0
     #for testing will use @ as command prefix
     commandprefix='@'
     
@@ -22,6 +24,37 @@ class TestBot(PyBot):
             self.saytochat('Python standalone bot welcomes all over hub!')
         
         return
+    
+    def DownloadFL(self,Bnick):
+        
+        ds = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ds.bind((self.botip, 55445))
+        print 'Started to listen port 55445'
+        self.saycommand('$ConnectToMe '+Bnick+' '+self.botip+':55445')
+        while 1:
+            ds.listen(1)
+            csock, caddr = ds.accept()
+            print 'accepted connection from '+str(caddr)
+            while 1:
+                
+                csock.send('$MyNick '+self.botnick+'|')
+                ct=readsock(csock)
+                print 'Sent my nick'
+                print 'Got '+str(ct)
+                while 1:
+                    t=readsock(csock)
+                    print 'Got '+t
+                    text=t.split()
+                    if text[0]=='$MyNick': csock.send('$Lock EXTENDEDPROTOCOLABCABCABCABCABCABC Pk=DCPLUSPLUS0.706ABCABC|')
+                    if text[0]=='$Lock':
+                        csock.send('$Lock '+lock2key2(text[1])+'|')
+                        csock.send('$Direction Download '+str(random.randint(1,100000))+'|')
+                    if text[0]=='$Key':
+                        csock.send('$ADCGET file files.xml.bz2 0 -1 ZL1|')
+                    #if text[0]=='':
+                    #random.randint(1,10000)
+        return
+    
     
     
     def workloop(self):
@@ -38,4 +71,6 @@ class TestBot(PyBot):
             
 tbot=TestBot()
 tbot.login()
-tbot.workloop()
+print 'Login Complete'
+tbot.DownloadFL('dr-evil')
+#tbot.workloop()
